06853448b269250aff4a438b9a59d52320c8a460
Kevin Tran <iicykdt@gmail.com>
2023-11-26 16:15:40 -0800

    Fixed Data Files, Add numeric column, and Summarized data

--- Final Project_Data Wrangling.R
+++ Final Project_Data Wrangling.R
@@ -1,10 +1,23 @@
 library(dplyr)
 library(stringr)
+library(ggplot2)
 
+# Deleted
+# share-healthy-diet-unaffordable.csv
 death_risk_df <- read.csv("number-of-deaths-by-risk-factor.csv")
-food_prices <- read.csv("share-healthy-diet-unaffordable.csv")
+food_prices_df <- read.csv("WLD_RTFP_country_2023-11-20 2.csv")
 
-df <- left_join(food_prices,death_risk_df, by = c("Entity", "Year"))
+# New numeric column parsing date and taking out Year
+food_prices_df$Year <- as.numeric(format(as.Date(food_prices_df$date, format="%Y-%m-%d"),"%Y"))
+# Summarizing food_prices_df taking avg_inflation and median_inflation per year for each country
+food_prices_df <- summarise(group_by(food_prices_df, country, date, Year), avg_inflation = mean(Inflation, na.rm = TRUE),
+                            med_inflation = median(Inflation, na.rm = TRUE))
+# New categorical column telling if avg_inflation rate is "positive", "negative", "no change"
+food_prices_df$inflation_trend <- summarise(food_prices_df, )
+
+df <- left_join(death_risk_df, food_prices_df, by = c("Entity" = "country", "Year" = "Year"))
+df <- filter(df, Year >= 2007)
+
 
 # Data Cleaning : Only have data from 2017-2019
 #print(df$Code.y)
